b6300d86b91571974ac57c81c7d429cb039e69cd
selvakumarj1807 <selvakumarj1807@gmail.com>
2025-10-07 11:58:31 +0530

    Python Modules

---
+++ PythonBasics/13ModuleCreation/demo.py
@@ -0,0 +1,7 @@
+import module
+
+print(module.studentName, ' your age is ', module.age)
+
+print('-------------------------------------------')
+
+module.display(10, 2)
---
+++ PythonBasics/13ModuleCreation/module.py
@@ -0,0 +1,10 @@
+studentName = "Alice"
+age = 20
+
+print('-----------------------------------------------------')
+
+def display(x, y):
+    print(x, ' + ', y, ' = ', x + y)
+    print(x, ' - ', y, ' = ', x - y)
+    print(x, ' * ', y, ' = ', x * y)
+    print(x, ' / ', y, ' = ', x / y)
